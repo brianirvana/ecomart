e3fc22b51b13ac1389b55cefcc86d13e7c3ee330
Brian Sabatier <brianirvana@gmail.com>
2025-11-08 15:56:48 -0300

    Validate input early so axios doesn't throw a confusing error when url is undefined

--- src/utilities/createResource.js
+++ src/utilities/createResource.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
 export const createResource = async (url, data, token) => {
+    // Validate URL early to avoid making requests to the dev server origin when url is undefined
+    if (!url) {
+        console.error('createResource called with undefined URL. Did you set VITE_SHOP or the correct VITE_ variable in your .env?');
+        throw new Error('Request URL is undefined. Check your VITE_ environment variables (e.g. VITE_SHOP).');
+    }
+
     try {
         const config = {
             headers: {
--- src/utilities/getResource.js
+++ src/utilities/getResource.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
 export const getResource = async (url, token) => {
+    // Validate input early so axios doesn't throw a confusing error when url is undefined
+    if (!url) {
+        console.error('getResource called with undefined URL. Did you set VITE_PRODUCT / VITE_CATEGORY in your .env?');
+        throw new Error('Request URL is undefined. Check your VITE_ environment variables (e.g. VITE_PRODUCT).');
+    }
+
     try {
         const config = {
             headers: {}
